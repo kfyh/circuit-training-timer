6a18fcf0add4aab6319970011a0161b709e4b5af
Kevin Hew <kevinhew@gmail.com>
2021-02-07 15:33:43 +0100

    Add missing import to selectorActions

--- src/actions/selectorActions.ts
+++ src/actions/selectorActions.ts
@@ -1,5 +1,6 @@
 import { ACTION_TYPES } from '../reducers/selectorReducer';
 import { StepCircuit } from '../types/circuits';
+import { ISelectorAction } from '../reducers/selectorReducer';
 
 export const addCircuit = (circuit: StepCircuit): ISelectorAction => ({
 	type: ACTION_TYPES.ADD_CIRCUIT,
