dcb1cd9b0db297e64146a0ae097797165c90ff5e
Kevin Hew <kevinhew@gmail.com>
2021-02-17 20:32:58 +0100

    selectable circuits

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -1,24 +1,48 @@
 import React, { ReactElement } from 'react';
 import { connect } from 'react-redux';
 import { History } from 'history';
-import { ISelectorReducerState } from 'src/reducers';
-import { Circuit } from 'src/types/circuits';
+import { ISelectorAction, ISelectorReducerState } from '../reducers';
+import { Circuit } from '../types/circuits';
+import { selectCircuit } from '../actions/selectorActions';
+import { Dispatch } from 'redux';
 
 type SelectorProps = {
 	circuits: Array<Circuit>;
 	history: History;
+	selectCircuit: (circuit: Circuit) => void;
 };
 
-export function Selector({ circuits, history }: SelectorProps): ReactElement {
-	return (
-		<div>
-			<div>Please Select a Circuit</div>
-			{circuits.map((circuit) => {
-				return <p>{circuit.name}</p>;
-			})}
-			<button onClick={() => history.push('/')}>Pick Me</button>
-		</div>
-	);
+export class Selector extends React.Component<SelectorProps, Record<string, unknown>> {
+	constructor(props: SelectorProps) {
+		super(props);
+	}
+
+	public render(): ReactElement {
+		const { circuits, history, selectCircuit } = this.props;
+
+		return (
+			<div>
+				<div>Please Select a Circuit</div>
+				{circuits.map((circuit) => {
+					return (
+						<div key={circuit.id}>
+							<p>{circuit.name}</p>
+							<button
+								onClick={(e) => {
+									e.preventDefault();
+									selectCircuit(circuit);
+									history.push('/');
+								}}
+							>
+								select
+							</button>
+						</div>
+					);
+				})}
+				<button onClick={() => history.push('/')}>Back</button>
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = (state: ISelectorReducerState) => {
@@ -27,4 +51,8 @@ const mapStateToProps = (state: ISelectorReducerState) => {
 	};
 };
 
-export const ConnectedSelector = connect(mapStateToProps)(Selector);
+const mapDispatchToProps = (dispatch: Dispatch<ISelectorAction>) => ({
+	selectCircuit: (circuit: Circuit) => dispatch(selectCircuit(circuit)),
+});
+
+export const ConnectedSelector = connect(mapStateToProps, mapDispatchToProps)(Selector);
