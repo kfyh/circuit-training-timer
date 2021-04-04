a7b8c860cac5f91255d675d86c307b5736c69a77
Kevin Hew <kevinhew@gmail.com>
2021-04-04 09:14:05 +0200

    Separate CircuitForm from AddCircuitPage

--- src/components/creator/AddCircuitPage.tsx
+++ src/components/creator/AddCircuitPage.tsx
@@ -6,8 +6,7 @@ import { History } from 'history';
 import { ExerciseGroup, Exercise, Circuit } from '../../types/circuits';
 import { ISelectorAction, ISelectorReducerState } from '../../reducers';
 import { addCircuit } from '../../actions/selectorActions';
-import { GroupView } from '../selector/GroupView';
-import { EditGroupView, EditGroupViewData } from './EditGroupView';
+import { CircuitForm, CircuitFormData } from './CircuitForm';
 
 type AddCircuitPageProps = {
 	exercises: Array<Exercise>;
@@ -19,8 +18,6 @@ type AddCircuitPageState = {
 	name: string;
 	exerciseGroups: Array<ExerciseGroup>;
 	repetitions: string;
-	selectedGroup: number;
-	newGroupName: string;
 };
 
 export class AddCircuitPage extends React.Component<AddCircuitPageProps, AddCircuitPageState> {
@@ -30,96 +27,12 @@ export class AddCircuitPage extends React.Component<AddCircuitPageProps, AddCirc
 			name: 'new circuit',
 			exerciseGroups: [],
 			repetitions: '1',
-			selectedGroup: -1,
-			newGroupName: 'Group Name',
 		};
 	}
-	private onNameChange = (e: FormEvent<HTMLInputElement>): void => {
-		e.preventDefault();
-		const name = e.currentTarget.value;
+
+	private onChange = (data: CircuitFormData): void => {
 		this.setState(() => {
-			return {
-				name,
-			};
-		});
-	};
-
-	private onRepetitionsChange = (e: FormEvent<HTMLInputElement>): void => {
-		e.preventDefault();
-		const repetitions = e.currentTarget.value;
-
-		this.setState(() => {
-			return {
-				repetitions,
-			};
-		});
-	};
-
-	private onNewGroupNameChange = (e: FormEvent<HTMLInputElement>): void => {
-		e.preventDefault();
-		const newGroupName = e.currentTarget.value;
-		if (this.state.newGroupName !== newGroupName) {
-			this.setState(() => {
-				return {
-					newGroupName,
-				};
-			});
-		}
-	};
-
-	private addGroup = (e: FormEvent<HTMLButtonElement>): void => {
-		e.preventDefault();
-		const name = this.state.newGroupName;
-		this.setState((state: AddCircuitPageState) => {
-			const exerciseGroups = [
-				...this.state.exerciseGroups,
-				{
-					name,
-					exercises: [],
-					repetitions: 1,
-					rest: 0,
-				},
-			];
-			return {
-				...state,
-				exerciseGroups,
-				selectedGroup: exerciseGroups.length - 1,
-			};
-		});
-	};
-
-	private onGroupChange = (index: number, data: EditGroupViewData, keepFocus: boolean) => {
-		const selectedGroup = keepFocus ? index : -1;
-		const editedGroup = {
-			name: data.name,
-			exercises: data.exercises,
-			repetitions: parseInt(data.repetitions),
-			rest: parseInt(data.rest)
-		};
-		this.setState((state: AddCircuitPageState) => {
-			const exerciseGroups = state.exerciseGroups.map((group, groupIndex) => {
-				if (groupIndex === index) {
-					return {
-						...group,
-						...editedGroup,
-					};
-				}
-
-				return group;
-			});
-
-			return {
-				exerciseGroups,
-				selectedGroup,
-			};
-		});
-	};
-
-	private selectGroup = (index: number) => {
-		this.setState(() => {
-			return {
-				selectedGroup: index,
-			};
+			return { ...data };
 		});
 	};
 
@@ -139,35 +52,7 @@ export class AddCircuitPage extends React.Component<AddCircuitPageProps, AddCirc
 	public render(): ReactElement {
 		return (
 			<div>
-				<h1>{this.state.name ? this.state.name : 'New Circuit'}</h1>
-				<input type="text" id="name" placeholder="Name" value={this.state.name} onChange={this.onNameChange} autoFocus />
-				<label htmlFor="repetitions">Repetitions</label>
-				<input type="text" id="repetitions" placeholder="Repetitions" value={this.state.repetitions} onChange={this.onRepetitionsChange} />
-				{this.state.exerciseGroups.map((group, index) => {
-					return this.state.selectedGroup === index ? (
-						<EditGroupView
-							key={index}
-							exerciseGroup={group}
-							exerciseStore={this.props.exercises}
-							onChange={(data: EditGroupViewData, keepFocus: boolean) => {
-								this.onGroupChange(index, data, keepFocus);
-							}}
-						/>
-					) : (
-						<GroupView
-							key={index}
-							group={group}
-							exerciseStore={this.props.exercises}
-							onClick={() => {
-								this.selectGroup(index);
-							}}
-						/>
-					);
-				})}
-				<div>
-					<input type="text" placeholder="Group" value={this.state.newGroupName} onChange={this.onNewGroupNameChange} />
-					<button onClick={this.addGroup}>Add Group</button>
-				</div>
+				<CircuitForm {...this.state} exercises={this.props.exercises} onChange={this.onChange} />
 				<button onClick={this.saveCircuit}>Save Circuit</button>
 			</div>
 		);
---
+++ src/components/creator/CircuitForm.tsx
@@ -0,0 +1,154 @@
+import React, { FormEvent, ReactElement } from 'react';
+
+import { ExerciseGroup, Exercise } from '../../types/circuits';
+import { GroupView } from '../selector/GroupView';
+import { EditGroupView, EditGroupViewData } from './EditGroupView';
+
+export type CircuitFormData = {
+	name?: string;
+	exerciseGroups?: Array<ExerciseGroup>;
+	repetitions?: string;
+};
+type CircuitFormProps = {
+	name: string;
+	exerciseGroups: Array<ExerciseGroup>;
+	repetitions: string;
+	exercises: Array<Exercise>;
+	onChange: (state: CircuitFormData) => void;
+};
+
+type CircuitFormState = {
+	name: string;
+	exerciseGroups: Array<ExerciseGroup>;
+	repetitions: string;
+	selectedGroup: number;
+	newGroupName: string;
+};
+
+export class CircuitForm extends React.Component<CircuitFormProps, CircuitFormState> {
+	constructor(props: CircuitFormProps) {
+		super(props);
+		this.state = {
+			name: props.name,
+			exerciseGroups: props.exerciseGroups,
+			repetitions: props.repetitions,
+			selectedGroup: -1,
+			newGroupName: 'Group Name',
+		};
+	}
+
+	private onNameChange = (e: FormEvent<HTMLInputElement>): void => {
+		e.preventDefault();
+		const name = e.currentTarget.value;
+		this.props.onChange({ name });
+	};
+
+	private onRepetitionsChange = (e: FormEvent<HTMLInputElement>): void => {
+		e.preventDefault();
+		const repetitions = e.currentTarget.value;
+		this.props.onChange({ repetitions });
+	};
+
+	private onNewGroupNameChange = (e: FormEvent<HTMLInputElement>): void => {
+		e.preventDefault();
+		const newGroupName = e.currentTarget.value;
+		if (this.state.newGroupName !== newGroupName) {
+			this.setState(() => {
+				return {
+					newGroupName,
+				};
+			});
+		}
+	};
+
+	private addGroup = (e: FormEvent<HTMLButtonElement>): void => {
+		e.preventDefault();
+		const name = this.state.newGroupName;
+		const exercise = {
+			name,
+			exercises: [],
+			repetitions: 1,
+			rest: 0,
+		};
+		const exerciseGroups = [...this.props.exerciseGroups, exercise];
+		this.props.onChange({ exerciseGroups });
+		this.setState(() => {
+			return {
+				selectedGroup: exerciseGroups.length - 1,
+			};
+		});
+	};
+
+	private onGroupChange = (index: number, data: EditGroupViewData, keepFocus: boolean) => {
+		const selectedGroup = keepFocus ? index : -1;
+		const editedGroup = {
+			name: data.name,
+			exercises: data.exercises,
+			repetitions: parseInt(data.repetitions),
+			rest: parseInt(data.rest),
+		};
+
+		const exerciseGroups = this.props.exerciseGroups.map((group, groupIndex) => {
+			if (groupIndex === index) {
+				return {
+					...group,
+					...editedGroup,
+				};
+			}
+
+			return group;
+		});
+
+		this.props.onChange({ exerciseGroups });
+
+		this.setState(() => {
+			return {
+				selectedGroup,
+			};
+		});
+	};
+
+	private selectGroup = (index: number) => {
+		this.setState(() => {
+			return {
+				selectedGroup: index,
+			};
+		});
+	};
+
+	public render(): ReactElement {
+		return (
+			<div>
+				<h1>{this.props.name ? this.props.name : 'New Circuit'}</h1>
+				<input type="text" id="name" placeholder="Name" value={this.props.name} onChange={this.onNameChange} autoFocus />
+				<label htmlFor="repetitions">Repetitions</label>
+				<input type="text" id="repetitions" placeholder="Repetitions" value={this.props.repetitions} onChange={this.onRepetitionsChange} />
+				{this.props.exerciseGroups.map((group, index) => {
+					return this.state.selectedGroup === index ? (
+						<EditGroupView
+							key={index}
+							exerciseGroup={group}
+							exerciseStore={this.props.exercises}
+							onChange={(data: EditGroupViewData, keepFocus: boolean) => {
+								this.onGroupChange(index, data, keepFocus);
+							}}
+						/>
+					) : (
+						<GroupView
+							key={index}
+							group={group}
+							exerciseStore={this.props.exercises}
+							onClick={() => {
+								this.selectGroup(index);
+							}}
+						/>
+					);
+				})}
+				<div>
+					<input type="text" placeholder="Group" value={this.state.newGroupName} onChange={this.onNewGroupNameChange} />
+					<button onClick={this.addGroup}>Add Group</button>
+				</div>
+			</div>
+		);
+	}
+}
